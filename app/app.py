e63cd874f170674605a7bec56d5445f1b223be9a
LimeHawk <kaikelfalcao@gmail.com>
2023-04-25 01:21:48 -0300

    Basic HTML Layout

--- app/app.py
+++ app/app.py
@@ -10,7 +10,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-  return render_template('index.html')
+  name = 'John Smith'
+  return render_template('index.html',name=name)
 
 if __name__ == '__main__':
   app.run() 
