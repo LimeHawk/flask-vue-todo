dd2cee71244d90f6b8b31fdc43eacf194196fe32
LimeHawk <kaikelfalcao@gmail.com>
2023-04-25 01:08:32 -0300

    Adding the templates and static folder

--- app/app.py
+++ app/app.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask import render_template
 
 from dotenv import load_dotenv
 
@@ -7,5 +8,9 @@ load_dotenv('./.flaskenv')
 
 app = Flask(__name__)
 
+@app.route('/')
+def index():
+  return render_template('index.html')
+
 if __name__ == '__main__':
-    app.run()
+  app.run() 
