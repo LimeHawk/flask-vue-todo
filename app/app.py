fdc253f97c3d474a7b78f8f0a7f68620f4aa98e5
LimeHawk <kaikelfalcao@gmail.com>
2023-04-25 03:07:31 -0300

    Creating Task model and installing sqlAlchemy

--- app/app.py
+++ app/app.py
@@ -1,17 +1,26 @@
 from flask import Flask
 from flask import render_template
+from flask import jsonify
 
 from dotenv import load_dotenv
+from flask_sqlalchemy import SQLAlchemy
+
+from config import Config
+import models
 
 
 load_dotenv('./.flaskenv')
 
 app = Flask(__name__)
+app.config.from_object(Config)
+
+db = SQLAlchemy(app)
 
 @app.route('/')
 def index():
-  name = 'John Smith'
-  return render_template('index.html',name=name)
+  tasks = models.Task.query.all()
+  #return render_template('index.html')
+  return jsonify(tasks)
 
 if __name__ == '__main__':
   app.run() 
